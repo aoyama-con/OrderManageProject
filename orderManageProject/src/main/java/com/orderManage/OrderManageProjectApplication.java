e537455ce8793364d1c618a4f16a30b781a2b81c
nakagawa <t.nakagawa@aoyama-con.jp>
2024-09-15 14:26:05 +0900

    20240915_Tomcatでのデプロイのために修正(中川)

--- orderManageProject/src/main/java/com/orderManage/OrderManageProjectApplication.java
+++ orderManageProject/src/main/java/com/orderManage/OrderManageProjectApplication.java
@@ -2,12 +2,19 @@ package com.orderManage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OrderManageProjectApplication {
+public class OrderManageProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManageProjectApplication.class, args);
 	}
 
+	/* 外部Tomcatで動作するために必要 20240901*/
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OrderManageProjectApplication.class);
+    }
 }
