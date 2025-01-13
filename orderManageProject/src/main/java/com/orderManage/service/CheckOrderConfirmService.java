7378e26c03147d75d7cdab7fe96d085ad19d3454
makabe <a.makabe@aoyama-con.jp>
2025-01-13 22:46:40 +0900

    ソースレビュー指摘事項対応（共通化、ラムダ式）

--- orderManageProject/src/main/java/com/orderManage/service/CheckOrderConfirmService.java
+++ orderManageProject/src/main/java/com/orderManage/service/CheckOrderConfirmService.java
@@ -21,15 +21,11 @@ import com.orderManage.controller.object.CheckOrderConfirmForm;
 import com.orderManage.controller.object.CheckOrderConfirmSubForm;
 import com.orderManage.model.api.OrderProducts;
 import com.orderManage.model.api.PurchaseOrdersInfo;
-import com.orderManage.model.api.StaffInfo;
 import com.orderManage.model.api.SuppliersInfo;
-import com.orderManage.model.param.ParamPurchaseOrderInfo;
-import com.orderManage.model.param.ParamStaffInfo;
 import com.orderManage.model.param.ParamSupplierInfo;
 import com.orderManage.model.session.SmarejiUser;
 import com.orderManage.util.SmarejiApiAccess;
 import com.orderManage.util.SmarejiApiAccessMock;
-import com.orderManage.util.StringUtil;
 
 /**
  * 発注内容確認画面画面サービスクラス
@@ -52,13 +48,7 @@ public class CheckOrderConfirmService extends OrderManageService {
 	/* スマレジAPIアクセスクラス定義 ※定義必須*/
 	@Autowired
 	SmarejiApiAccessMock smarejiApiAccessMock;
-	
-	/* ステータス(5:仮発注) */
-    private final String STATUS_TENTATIVE_ORDER = "5";
-	
-	/* 発注データ取得上限件数 */
-    private final int DATA_LIMIT_CNT = 1000;
-    
+
 	/**
 	 * 仕入先情報を取得
 	 * 
@@ -84,10 +74,6 @@ public class CheckOrderConfirmService extends OrderManageService {
 		
 		// 仕入先一覧を取得(API)
 		supplierInfoList = smarejiApiAccess.getSuppliersInfo(smarejiUser.getContract().getId(), param);
-		/** テスト用 ローカルで動かす用mockを使用 *****************/ 
-		// テスト用の仕入先一覧取得
-		//supplierInfoList = smarejiApiAccessMock.getSuppliersInfo("dummyid");
-		/***********************************************/
 		
 		Iterator<SuppliersInfo> it = supplierInfoList.iterator();
 		Map<String, String> supplierInfoMap = new LinkedHashMap<String, String>();
@@ -100,163 +86,6 @@ public class CheckOrderConfirmService extends OrderManageService {
 		logger.info("getSupplierInfo:仕入先情報取得　処理終了");
 		
 		return supplierInfoMap;
-	}
-	
-	/**
-	 * スタッフ情報を取得
-	 * 
-	 * スタッフ一覧取得APIを使用してスタッフコード・スタッフ名を取得する
-	 * 
-	 * @param smarejiUser スマレジユーザ情報
-	 * @return Map スタッフコード・スタッフ名
-	 */
-	public Map<String, String> getStaffInfo(SmarejiUser smarejiUser) {
-		
-		logger.info("getStaffInfo:スタッフ一覧取得　処理開始");
-
-		List<StaffInfo> staffInfoList = new ArrayList<StaffInfo>();
-
-		// スタッフ一覧取得パラメータを設定
-		ParamStaffInfo param = new ParamStaffInfo();		
-		
-		// 取得条件
-		String staffId = smarejiUser.getContract().getUser_id();
-		if (!StringUtil.isEmpty(staffId)) {			
-			param.setStaff_id(Integer.parseInt(staffId));	// スタッフIDが取得できた場合のみ条件に設定
-		}		
-		
-		// 取得項目
-		List<String> getParam = new ArrayList<String>();
-		getParam.add("staffId");
-		getParam.add("staffName");
-		param.setFields(getParam);
-		
-		// スタッフ一覧を取得(API)
-		staffInfoList = smarejiApiAccess.getStaffsInfo(smarejiUser.getContract().getId(), param);
-		/** テスト用 ローカルで動かす用mockを使用 *****************/ 
-		// テスト用のスタッフ一覧取得
-		//staffInfoList = smarejiApiAccessMock.getStaffInfo("dummyid");
-		/***********************************************/
-		
-		Iterator<StaffInfo> it = staffInfoList.iterator();
-		Map<String, String> staffInfoMap = new LinkedHashMap<String, String>();
-		while (it.hasNext()) {
-			// スタッフコード、スタッフ名の設定
-			StaffInfo staff = it.next();
-			staffInfoMap.put(staff.getStaffId(), staff.getStaffName());
-		}
-		
-		logger.info("getStaffInfo:スタッフ一覧取得　処理終了");
-		
-		return staffInfoMap;
-	}
-	
-	/**
-	 * 発注一覧情報を取得
-	 * 
-	 * 発注一覧取得APIを使用して発注情報を取得する
-	 * 
-	 * @param smarejiUser スマレジユーザ情報
-	 * @param orderControlNumber 前画面から受け取った発注管理番号
-	 * @return List 発注一覧情報
-	 */
-	public List<PurchaseOrdersInfo> getPurchaseOrdersInfoList(SmarejiUser smarejiUser, String orderControlNumber) {
-		
-		logger.info("getPurchaseOrdersInfo:発注一覧取得　処理開始");
-		
-		List<PurchaseOrdersInfo> purchaseOrdersInfoList = new ArrayList<PurchaseOrdersInfo>();
-		
-		// 発注一覧取得パラメータを設定
-		ParamPurchaseOrderInfo param = new ParamPurchaseOrderInfo();
-		
-		// 取得条件
-		param.setStatus(STATUS_TENTATIVE_ORDER); // ステータス（5：仮発注）
-		
-		// 取得項目
-		List<String> getParam = new ArrayList<String>();
-		getParam.add("storageInfoId");
-		getParam.add("identificationNo"); // 発注管理番号
-		param.setFields(getParam);
-		
-		// 取得件数
-		param.setLimit(DATA_LIMIT_CNT);
-		
-		// ソート順（発注日（昇順）、発注ID（昇順））
-		param.setSort("orderedDate:asc," + "storageInfoId:asc");
-		
-		// 発注一覧を取得(API)
-		purchaseOrdersInfoList = smarejiApiAccess.getPurchaseOrdersInfo(smarejiUser.getContract().getId(), param);
-		logger.info("発注一覧取得API取得件数：" + purchaseOrdersInfoList.size());
-		/** テスト用 ローカルで動かす用mockを使用 *****************/ 
-		// テスト用の発注一覧取得
-		//purchaseOrdersInfoList = smarejiApiAccessMock.getPurchaseOrdersInfo("dummyid");
-		/***********************************************/
-		
-		// パラメータの発注管理番号と一致するデータのみに絞り込み		
-		List<PurchaseOrdersInfo> resultList = new ArrayList<PurchaseOrdersInfo>();
-		for(PurchaseOrdersInfo orderInfo : purchaseOrdersInfoList) {
-			if (!orderControlNumber.equals(orderInfo.getIdentificationNo())) {
-				continue;
-			}
-
-			PurchaseOrdersInfo resultModel = new PurchaseOrdersInfo();
-			resultModel.setStorageInfoId(orderInfo.getStorageInfoId());
-			resultList.add(resultModel);
-		}
-		logger.info("発注管理番号：" + orderControlNumber + "、" + "発注取得件数：" + resultList.size());
-		
-		logger.info("getPurchaseOrdersInfo:発注一覧報取得　処理終了");
-		
-		return resultList;
-	}
-	
-	/**
-	 * 発注情報を取得
-	 * 
-	 * 発注取得APIを使用して発注情報を取得する
-	 * 
-	 * @param smarejiUser スマレジユーザ情報
-	 * @param storageInfoId 発注ID
-	 * @return PurchaseOrdersInfo 発注情報
-	 */
-	public PurchaseOrdersInfo getPurchaseOrdersInfo(SmarejiUser smarejiUser, String storageInfoId) {
-		
-		logger.info("getSupplierInfo:発注情報取得　処理開始");
-				
-		PurchaseOrdersInfo purchaseOrdersInfo = new PurchaseOrdersInfo();
-		
-		// 発注情報取得パラメータを設定
-		ParamPurchaseOrderInfo param = new ParamPurchaseOrderInfo();
-		
-		// 取得条件
-		param.setWith_products("all"); // 発注対象商品、発注配送商品を付加する
-		
-		// 取得項目
-		List<String> getParam = new ArrayList<String>();
-		getParam.add("storageInfoId");
-		getParam.add("recipientOrderId");
-		getParam.add("orderedDate");
-		getParam.add("staffId");
-		param.setFields(getParam);
-		
-		// 発注情報を取得(API)	  
-		purchaseOrdersInfo = smarejiApiAccess.getPurchaseOrderInfo(smarejiUser.getContract().getId(), storageInfoId, param);
-		/** テスト用 ローカルで動かす用mockを使用 *****************/ 
-		// テスト用の発注情報取得		
-		//purchaseOrdersInfo = smarejiApiAccessMock.getPurchaseOrderInfo("dummyid");
-		/***********************************************/
-		/* TODO APIのResponsesコードを判定する　ステータスコード=429の場合｢Retry-After｣に待機時間(秒数)が返されるのでその間待機し再度クエリーを行うようにする(☞ステータスチェックと待機・リトライは汎用部品化することが望ましい)。
-		if (終了条件) {
-		    return 戻り値;
-		} else {
-		    メソッド名(引数);
-		    return 戻り値;
-		}
-		*/
-	
-		logger.info("getPurchaseOrdersInfo:発注情報取得　処理終了");
-		
-		return purchaseOrdersInfo;
 	}
 	
 	/**
@@ -281,8 +110,38 @@ public class CheckOrderConfirmService extends OrderManageService {
 		
 		// 発注一覧取得API（取得条件：契約ID、ステータス"5：仮発注"、発注管理番号）
 		List<PurchaseOrdersInfo> purchaseOrdersInfoList = getPurchaseOrdersInfoList(smarejiUser, orderControlNumber);
-		
+			
 		// 発注一覧の取得件数分、以下の処理を実施
+		// TODO ラムダ式とstreamAPIにする
+		/*		purchaseOrdersInfoList.forEach(order -> {
+					// 発注情報取得API（取得条件：発注ID）
+					PurchaseOrdersInfo purchaseOrdersInfo = getPurchaseOrdersInfo(smarejiUser, order.getStorageInfoId());
+					
+					// 発注商品の数量と金額の合計を取得
+					BigDecimal countSum = new BigDecimal("0");
+					BigDecimal amountSum = new BigDecimal("0");
+		
+					for (OrderProducts product : purchaseOrdersInfo.getProducts()){				
+						BigDecimal count = new BigDecimal(product.getQuantity());
+						BigDecimal cost = new BigDecimal(product.getCost());
+						countSum = countSum.add(count);
+						amountSum = amountSum.add(cost.multiply(count)); 
+					}
+					
+					// 名称取得（仕入先）
+					String supplierName = supplierMap.get(purchaseOrdersInfo.getRecipientOrderId());
+					
+					// 画面表示用Formに情報を設定
+					DecimalFormat decimalFormat = new DecimalFormat("#,###.#");		
+					CheckOrderConfirmSubForm subForm = new CheckOrderConfirmSubForm();
+					subForm.setOrderId(purchaseOrdersInfo.getStorageInfoId());		// 発注ID
+					subForm.setOrderedDate(purchaseOrdersInfo.getOrderedDate());	// 発注日		
+					subForm.setSupplierName(supplierName);							// 仕入先名		
+					subForm.setOrderStaffName(staffName);							// 発注者名（ログインユーザ名）
+					subForm.setOrderCount(decimalFormat.format(countSum));			// 発注点数
+					subForm.setOrderAmountSum(decimalFormat.format(amountSum.setScale(1, RoundingMode.HALF_UP))); // 発注金額合計（小数点以下第二位を四捨五入）
+					displayList.add(subForm);
+				});*/
 		for (PurchaseOrdersInfo order : purchaseOrdersInfoList){
 
 			// 発注情報取得API（取得条件：発注ID）
@@ -314,7 +173,7 @@ public class CheckOrderConfirmService extends OrderManageService {
 			subForm.setOrderAmountSum(decimalFormat.format(amountSum.setScale(1, RoundingMode.HALF_UP))); // 発注金額合計（小数点以下第二位を四捨五入）
 			displayList.add(subForm);
 		}
-
+		
 		checkOrderConfirmForm.setDisplayList(displayList);
 		
 		logger.info("getDisplayInfoList:発注確認画面初期表示取得　処理終了");
--- orderManageProject/src/main/java/com/orderManage/service/OrderManageService.java
+++ orderManageProject/src/main/java/com/orderManage/service/OrderManageService.java
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Service;
 
 import com.orderManage.model.api.CategorieInfo;
 import com.orderManage.model.api.ProductImageInfo;
+import com.orderManage.model.api.PurchaseOrdersInfo;
 import com.orderManage.model.api.StockInfo;
 import com.orderManage.model.param.ParamCategorieInfo;
 import com.orderManage.model.param.ParamProductImage;
+import com.orderManage.model.param.ParamPurchaseOrderInfo;
 import com.orderManage.model.param.ParamStockInfo;
 import com.orderManage.model.session.SmarejiUser;
 import com.orderManage.util.SmarejiApiAccess;
@@ -50,6 +52,9 @@ abstract class OrderManageService {
 	/** APIループ上限数 */
 	protected static int API_LOOP_COUNT = 100;
 	
+	/** ステータス(5:仮発注) **/
+	protected static String STATUS_TENTATIVE_ORDER = "5";
+
 	/**
 	 * 商品画像一覧を取得
 	 * 
@@ -209,5 +214,117 @@ abstract class OrderManageService {
 		logger.info("getStocksInfo:在庫一覧取得　処理終了");
 		
 		return stocksInfoMap;
+	}
+	
+	/**
+	 * 発注一覧情報を取得
+	 * 
+	 * 発注一覧取得APIを使用して発注情報を取得する
+	 * 
+	 * @param smarejiUser スマレジユーザ情報
+	 * @param orderControlNumber 前画面から受け取った発注管理番号
+	 * @return List 発注一覧情報
+	 */
+	public List<PurchaseOrdersInfo> getPurchaseOrdersInfoList(SmarejiUser smarejiUser, String orderControlNumber) {
+		
+		logger.info("getPurchaseOrdersInfo:発注一覧取得　処理開始");
+		
+		List<PurchaseOrdersInfo> purchaseOrdersInfoList = new ArrayList<PurchaseOrdersInfo>();
+		
+		// 発注一覧取得パラメータを設定
+		ParamPurchaseOrderInfo param = new ParamPurchaseOrderInfo();
+		
+		// 取得条件
+		param.setStatus(STATUS_TENTATIVE_ORDER); // ステータス（5：仮発注）
+		
+		// 取得項目
+		List<String> getParam = new ArrayList<String>();
+		getParam.add("storageInfoId");
+		getParam.add("identificationNo"); // 発注管理番号
+		param.setFields(getParam);
+		
+		// 取得件数
+		param.setLimit(API_LIMIT);
+		
+		// ソート順（発注日（昇順）、発注ID（昇順））
+		param.setSort("orderedDate:asc," + "storageInfoId:asc");
+		
+		// 発注一覧を取得(API)
+		purchaseOrdersInfoList = smarejiApiAccess.getPurchaseOrdersInfo(smarejiUser.getContract().getId(), param);
+		logger.info("発注一覧取得API取得件数：" + purchaseOrdersInfoList.size());
+		/** テスト用 ローカルで動かす用mockを使用 *****************/ 
+		// テスト用の発注一覧取得
+		//purchaseOrdersInfoList = smarejiApiAccessMock.getPurchaseOrdersInfo("dummyid");
+		/***********************************************/
+		
+		// パラメータの発注管理番号と一致するデータのみに絞り込み	
+		List<PurchaseOrdersInfo> resultList = new ArrayList<PurchaseOrdersInfo>();
+		purchaseOrdersInfoList.stream().filter(x -> orderControlNumber.equals(x.getIdentificationNo()))
+				.collect(Collectors.toList()).stream().forEach(z -> {
+					PurchaseOrdersInfo resultModel = new PurchaseOrdersInfo();
+					resultModel.setStorageInfoId(z.getStorageInfoId());
+					resultList.add(resultModel);
+				});
+		
+		/*		List<PurchaseOrdersInfo> resultList = new ArrayList<PurchaseOrdersInfo>();
+				for(PurchaseOrdersInfo orderInfo : purchaseOrdersInfoList) {
+					if (!orderControlNumber.equals(orderInfo.getIdentificationNo())) {
+						continue;
+					}
+		
+					PurchaseOrdersInfo resultModel = new PurchaseOrdersInfo();
+					resultModel.setStorageInfoId(orderInfo.getStorageInfoId());
+					resultList.add(resultModel);
+				}*/
+		logger.info("発注管理番号：" + orderControlNumber + "、" + "発注取得件数：" + resultList.size());
+		
+		logger.info("getPurchaseOrdersInfo:発注一覧報取得　処理終了");
+		
+		return resultList;
+	}
+	
+	/**
+	 * 発注情報を取得
+	 * 
+	 * 発注取得APIを使用して発注情報を取得する
+	 * 
+	 * @param smarejiUser スマレジユーザ情報
+	 * @param storageInfoId 発注ID
+	 * @return PurchaseOrdersInfo 発注情報
+	 */
+	public PurchaseOrdersInfo getPurchaseOrdersInfo(SmarejiUser smarejiUser, String storageInfoId) {
+		
+		logger.info("getSupplierInfo:発注情報取得　処理開始");
+				
+		PurchaseOrdersInfo purchaseOrdersInfo = new PurchaseOrdersInfo();
+		
+		// 発注情報取得パラメータを設定
+		ParamPurchaseOrderInfo param = new ParamPurchaseOrderInfo();
+		
+		// 取得条件
+		param.setWith_products("all"); // 発注対象商品、発注配送商品を付加する
+		
+		// 取得項目
+		List<String> getParam = new ArrayList<String>();
+		getParam.add("storageInfoId");
+		getParam.add("recipientOrderId");
+		getParam.add("orderedDate");
+		getParam.add("staffId");
+		param.setFields(getParam);
+		
+		// 発注情報を取得(API)	  
+		purchaseOrdersInfo = smarejiApiAccess.getPurchaseOrderInfo(smarejiUser.getContract().getId(), storageInfoId, param);
+		/* TODO APIのResponsesコードを判定する　ステータスコード=429の場合｢Retry-After｣に待機時間(秒数)が返されるのでその間待機し再度クエリーを行うようにする(☞ステータスチェックと待機・リトライは汎用部品化することが望ましい)。
+		if (終了条件) {
+		    return 戻り値;
+		} else {
+		    メソッド名(引数);
+		    return 戻り値;
+		}
+		*/
+	
+		logger.info("getPurchaseOrdersInfo:発注情報取得　処理終了");
+		
+		return purchaseOrdersInfo;
 	}
 }
